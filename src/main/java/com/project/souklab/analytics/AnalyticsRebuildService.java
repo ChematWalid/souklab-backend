b689c78e195e540459e7e1c26274712537eb8e79
Your Name <chemat_oualid@univ-blida.dz>
2026-09-19 15:07:47 +0100

    type rollup event keys

--- src/main/java/com/project/souklab/analytics/AnalyticsRebuildService.java
+++ src/main/java/com/project/souklab/analytics/AnalyticsRebuildService.java
@@ -51,7 +51,8 @@ public class AnalyticsRebuildService {
             for (ActivityEvent event : source) {
                 LocalDate eventDay = event.getEventTime().toInstant(ZoneOffset.UTC)
                         .atZone(businessZone).toLocalDate();
-                String key = eventDay + "\u0000event." + event.getEventType().value();
+                String key = eventDay + "\u0000" + AnalyticsMetric.EventRollup.PREFIX.value()
+                        + event.getEventType().value();
                 counts.merge(key, 1L, Long::sum);
             }
         } while (source.size() == properties.getRollupBatchSize());
